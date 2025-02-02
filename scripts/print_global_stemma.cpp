5102d79bf7957dc2047d32857352f08cde9580a3
jjmccollum <jpmccollum05834@yahoo.com>
2025-02-03 02:56:09 +1100

    :zap: Added -e option (for specifying witnesses to exclude) and -p option (for specifiying minimum proportion of extant variation units) to various scripts

--- scripts/print_global_stemma.cpp
+++ scripts/print_global_stemma.cpp
@@ -12,6 +12,7 @@
 #include <sys/stat.h>
 #include <iostream>
 #include <fstream>
+#include <cmath>
 #include <string>
 #include <list>
 #include <vector>
@@ -44,8 +45,9 @@ int create_dir(const string & dir) {
 /**
  * Retrieves all rows from the WITNESSES table of the given SQLite database
  * and returns a list of witness IDs populated with its contents.
+ * Any witnesses whose IDs are in the set of excluded witness IDs will not be added to the witness list.
  */
-list<string> get_list_wit(sqlite3 * input_db) {
+list<string> get_list_wit(sqlite3 * input_db, set<string> & excluded_wit_ids) {
 	list<string> list_wit = list<string>();
 	int rc; //to store SQLite macros
 	sqlite3_stmt * select_from_witnesses_stmt;
@@ -53,6 +55,11 @@ list<string> get_list_wit(sqlite3 * input_db) {
 	rc = sqlite3_step(select_from_witnesses_stmt);
 	while (rc == SQLITE_ROW) {
 		string wit_id = string(reinterpret_cast<const char *>(sqlite3_column_text(select_from_witnesses_stmt, 0)));
+		//If the witness's ID is in the excluded set, then skip it:
+		if (excluded_wit_ids.find(wit_id) != excluded_wit_ids.end()) {
+			rc = sqlite3_step(select_from_witnesses_stmt);
+			continue;
+		}
 		list_wit.push_back(wit_id);
 		rc = sqlite3_step(select_from_witnesses_stmt);
 	}
@@ -61,10 +68,53 @@ list<string> get_list_wit(sqlite3 * input_db) {
 }
 
 /**
+ * Retrieves all rows from the VARIATION_UNITS table of the given SQLite database
+ * and returns a vector of variation unit labels populated with its contents.
+ */
+vector<string> get_variation_unit_labels(sqlite3 * input_db) {
+	vector<string> variation_unit_labels = vector<string>();
+	int rc; //to store SQLite macros
+	sqlite3_stmt * select_from_variation_units_stmt;
+	sqlite3_prepare(input_db, "SELECT LABEL FROM VARIATION_UNITS ORDER BY ROW_ID", -1, & select_from_variation_units_stmt, 0);
+	rc = sqlite3_step(select_from_variation_units_stmt);
+	while (rc == SQLITE_ROW) {
+		string label = string(reinterpret_cast<const char *>(sqlite3_column_text(select_from_variation_units_stmt, 0)));
+		variation_unit_labels.push_back(label);
+		rc = sqlite3_step(select_from_variation_units_stmt);
+	}
+	sqlite3_finalize(select_from_variation_units_stmt);
+	return variation_unit_labels;
+}
+
+/**
+ * Retrieves all rows from the GENEALOGICAL_COMPARISONS table of the given SQLite database
+ * and adds the IDs of all witnesses extant at fewer than the given number of variation units to the given set of excluded witness IDs.
+ */
+void add_fragmentary_witnesses_to_excluded_set(sqlite3 * input_db, const int min_extant, set<string> & excluded_wit_ids) {
+	int rc; //to store SQLite macros
+	sqlite3_stmt * select_from_genealogical_comparisons_stmt;
+	sqlite3_prepare(input_db, "SELECT * FROM GENEALOGICAL_COMPARISONS WHERE PRIMARY_WIT=SECONDARY_WIT ORDER BY ROW_ID", -1, & select_from_genealogical_comparisons_stmt, 0);
+	rc = sqlite3_step(select_from_genealogical_comparisons_stmt);
+	while (rc == SQLITE_ROW) {
+		string primary_wit_id = string(reinterpret_cast<const char *>(sqlite3_column_text(select_from_genealogical_comparisons_stmt, 1)));
+		int extant_bytes = sqlite3_column_bytes(select_from_genealogical_comparisons_stmt, 3);
+		const char * extant_buf = reinterpret_cast<const char *>(sqlite3_column_blob(select_from_genealogical_comparisons_stmt, 3));
+		Roaring extant = Roaring::readSafe(extant_buf, extant_bytes);
+		if (extant.cardinality() < min_extant) {
+			excluded_wit_ids.insert(primary_wit_id);
+		}
+		rc = sqlite3_step(select_from_genealogical_comparisons_stmt);
+	}
+	sqlite3_finalize(select_from_genealogical_comparisons_stmt);
+	return;
+}
+
+/**
  * Using rows for the given witness ID from the GENEALOGICAL_COMPARISONS table of the given SQLite database,
  * returns a witness.
+ * Any witnesses whose IDs are in the set of excluded witness IDs will not have their genealogical comparisons added to the witness being populated.
  */
-witness get_witness(sqlite3 * input_db, const string & wit_id) {
+witness get_witness(sqlite3 * input_db, const string & wit_id, set<string> & excluded_wit_ids) {
 	int rc; //to store SQLite macros
 	//Populate this witness's list of genealogical comparisons to other witnesses:
 	list<genealogical_comparison> comps = list<genealogical_comparison>();
@@ -78,6 +128,11 @@ witness get_witness(sqlite3 * input_db, const string & wit_id) {
 		comp.primary_wit = primary_wit_id;
 		string secondary_wit_id = string(reinterpret_cast<const char *>(sqlite3_column_text(select_from_genealogical_comparisons_stmt, 2)));
 		comp.secondary_wit = secondary_wit_id;
+		//If the secondary witness's ID is in the excluded set, then skip it:
+		if (excluded_wit_ids.find(secondary_wit_id) != excluded_wit_ids.end()) {
+			rc = sqlite3_step(select_from_genealogical_comparisons_stmt);
+			continue;
+		}
 		int extant_bytes = sqlite3_column_bytes(select_from_genealogical_comparisons_stmt, 3);
 		const char * extant_buf = reinterpret_cast<const char *>(sqlite3_column_blob(select_from_genealogical_comparisons_stmt, 3));
 		Roaring extant = Roaring::readSafe(extant_buf, extant_bytes);
@@ -121,15 +176,19 @@ witness get_witness(sqlite3 * input_db, const string & wit_id) {
  */
 int main(int argc, char* argv[]) {
 	//Read in the command-line options:
+	set<string> excluded_wit_ids = set<string>();
+	float proportion_extant = 0.0;
 	bool print_lengths = false;
 	bool flow_strengths = false;
 	string input_db_name = string();
 	try {
 		cxxopts::Options options("print_global_stemma", "Print a global stemma graph to a .dot output files. The output file will be placed in the \"global\" directory.");
-		options.custom_help("[-h] [--lengths] [--strengths] input_db");
+		options.custom_help("[-h] [-e wit_1 -e wit_2 ...] [-p proportion] [--lengths] [--strengths] input_db");
 		options.positional_help("").show_positional_help();
 		options.add_options("")
 				("h,help", "print this help")
+				("e,excluded", "IDs of witnesses to exclude from the global stemma", cxxopts::value<vector<string>>())
+				("p,proportion_extant", "minimum proportion of variation units at which a witness must be extant to be included in the global stemma", cxxopts::value<float>())
 				("lengths", "print genealogical costs as edge lengths")
 				("strengths", "format edges to reflect flow strengths");
 		options.add_options("positional")
@@ -140,6 +199,20 @@ int main(int argc, char* argv[]) {
 		if (args.count("help")) {
 			cout << options.help({""}) << endl;
 			exit(0);
+		}
+		if (args.count("e")) {
+			vector<string> excluded_witnesses = args["e"].as<vector<string>>();
+			for (string excluded_wit_id : excluded_witnesses) {
+				excluded_wit_ids.insert(excluded_wit_id);
+			}
+		}
+		if (args.count("p")) {
+			proportion_extant = args["p"].as<float>();
+			//Ensure that the input is between 0 and 1:
+			if (proportion_extant < 0.0 || proportion_extant > 1.0) {
+				cerr << "Error: The proportion of extant variation units " << proportion_extant << " is not between 0 and 1." << endl;
+				exit(1);
+			}
 		}
 		if (args.count("lengths")) {
 			print_lengths = args["lengths"].as<bool>();
@@ -168,14 +241,28 @@ int main(int argc, char* argv[]) {
 		cerr << "Error opening database " << input_db_name << ": " << sqlite3_errmsg(input_db) << endl;
 		exit(1);
 	}
+	//If the minimum extant proportion option has been specified, 
+	//then count the number of variation units, calculate the minimum number of extant units from this,
+	//and add all witnesses below this threshold to the set of excluded witnesses:
+	if (proportion_extant > 0.0) {
+		cout << "Calculating minimum number of extant variation units..." << endl;
+		vector<string> vu_labels = get_variation_unit_labels(input_db);
+		int min_extant = (int) ceil(proportion_extant * vu_labels.size());
+		cout << "Adding fragmentary witnesses to exclusion set..." << endl;
+		add_fragmentary_witnesses_to_excluded_set(input_db, min_extant, excluded_wit_ids);
+	}
 	cout << "Retrieving witness list..." << endl;
-	//Retrieve all witness IDs in the order in which they occur in the table:
-	list<string> list_wit = get_list_wit(input_db);
+	//Retrieve all witness IDs (for non-excluded witnesses) in the order in which they occur in the table:
+	list<string> list_wit = get_list_wit(input_db, excluded_wit_ids);
 	cout << "Initializing all witnesses..." << endl;
 	//Populate a list of witnesses:
 	list<witness> witnesses = list<witness>();
 	for (string wit_id : list_wit) {
-		witness wit = get_witness(input_db, wit_id);
+		//Do not add any witnesses in the excluded set:
+		if (excluded_wit_ids.find(wit_id) != excluded_wit_ids.end()) {
+			continue;
+		}
+		witness wit = get_witness(input_db, wit_id, excluded_wit_ids);
 		witnesses.push_back(wit);
 	}
 	//Close the database:
